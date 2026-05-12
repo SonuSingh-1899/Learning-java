2b59bc41d9dd2e44f144bff05948c591d8636406
Sonu Singh <shadow.pentest1899@gmail.com>
2026-05-12 11:05:02 +0530

    learn about bit wise operators

---
+++ Bitwiseoperator/bitwiseoperator.java
@@ -0,0 +1,15 @@
+package Bitwiseoperator;
+
+public class bitwiseoperator {
+    // operands --> byte short int long
+
+    // operators
+    // and operator &
+    // or operator |
+    // xor ^
+    // not ~
+    // left shift <<
+    // right shift >>
+    // unsigned right shift >>>
+    
+}
